f47b313f3127b82178e1f72955392e055eadb15c
Knyazik01 <viktor.kn@icloud.com>
2024-02-11 16:36:10 +0200

    - remove useless comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # INFO: favourite_ads cannot be edit: only view, create and delete
   resources :favourite_ads, only: [:index, :show, :create, :destroy]
-  # TODO add routes for favourite_ads:
-  #  - add/remove from favorite (with auth user)
-  #  - show my favourite_ads (only favorite ads for auth user)
 
   # resources :users # control under users should be opened for admins
   controller :users do
